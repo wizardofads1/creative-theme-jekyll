a169ff3c4ba5bbdaa093d5ec520072af2a64eeb3
boyanq <boyan@exceptcreate.com>
2023-10-29 12:28:21 +0200

    TinaCMS test version
    Might revert to previous version if this does not work

---
+++ tina/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "b279c119-7be8-4f8b-9ab0-33b00930e3f2", // Get this from tina.io
+  token: "3b41a86065b14761d41112b8c12ea97990237e18", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "./",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "./",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
